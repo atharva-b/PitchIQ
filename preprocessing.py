d068cce8eaf8af90472f1dec157f49e6719442ac
Atharva <atharva.bhalerao@gmail.com>
2025-11-11 13:47:05 -0500

    adding further lag features, removing days rest (might be noise) - increases accuracy by ~4%

--- preprocessing.py
+++ preprocessing.py
@@ -24,19 +24,31 @@ data.sort_values(['game_date', 'at_bat_number', 'pitch_number'], inplace=True)
 
 data['run_diff'] = data['fld_score'] - data['bat_score']  # create run_diff column
 
-# create prev_* columns
+# create lag columns
 group_cols = ['game_date', 'at_bat_number']
-for col in ['pitch_type', 'release_speed', 'release_spin_rate'] :
+for col in ['release_speed', 'release_spin_rate'] :
     data[f'prev_{col}'] = data.groupby(group_cols)[col].shift(1)
 
-data.dropna(subset=['prev_pitch_type'], inplace=True)
-data = pd.get_dummies(data, columns=['prev_pitch_type', 'stand', 'p_throws']) # one-hot encoding for these columns
+lag_columns = []
+for lag in [1, 2, 3] :
+    lag_col_name = f'prev_pitch_type_lag{lag}'
+    data[lag_col_name] = (
+        data.groupby(group_cols)['pitch_type']
+        .shift(lag)
+        .fillna('None')   # keep instead of dropping
+    )
+    lag_columns.append(lag_col_name)
+    
+
+data.dropna(subset=lag_columns, inplace=True)
+one_hot_columns = lag_columns + ['stand', 'p_throws']
+data = pd.get_dummies(data, columns=one_hot_columns) # one-hot encoding for these columns
 
 data['runners_on'] = data[['on_1b', 'on_2b', 'on_3b']].sum(axis=1)
 
 # numerical feature processing
 numerical_cols = ['prev_release_speed', 'prev_release_spin_rate', 
-    'balls', 'strikes', 'outs_when_up', 'at_bat_number', 'pitch_number', 'pitcher_days_since_prev_game',
+    'balls', 'strikes', 'outs_when_up', 'at_bat_number', 'pitch_number',
     'run_diff', 'runners_on']
 
 scaler = StandardScaler()
@@ -44,8 +56,8 @@ data[numerical_cols] = scaler.fit_transform(data[numerical_cols])
 
 keep_cols = ['pitch_type', 'game_date', 'balls', 'strikes', 'outs_when_up', 'inning', 'p_throws', 'stand',
     'at_bat_number', 'pitch_number',
-    'pitcher_days_since_prev_game', 'run_diff', 'runners_on',
-    'prev_pitch_type', 'prev_release_speed', 'prev_release_spin_rate'
+    'run_diff', 'runners_on',
+    'prev_release_speed', 'prev_release_spin_rate'
 ]
 prefixes = ['prev_pitch_type_', 'stand_', 'p_throws_']
 extra_cols = [c for c in data.columns if any(c.startswith(p) for p in prefixes)]
